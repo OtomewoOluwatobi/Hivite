02805089535471e856fe27c6189c637b1de88919
Otomewo Oluwatobi. E <otomewooluwatobi@gmail.com>
2024-08-16 22:40:54 +0100

    update on routes

---
+++ app/Http/Controllers/RolePermissionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionController extends Controller
+{
+    public function __construct(){
+    }
+
+    function store_role(Request $req){
+        $req->validate([
+            'name' => 'required|string|unique:roles,name',
+            "permission_ids" => "required|array",
+            "permission.*" => "required|numeric|exists:permissions,id"
+        ]);
+        $newRole = Role::create([
+            'name' => strtolower($req->input('name'))
+        ]);
+
+        $syncRole = $newRole->syncPermissions($req->permission_ids);
+
+        return response()->json([
+            "msg" => "permissions created successfully",
+            "data" => $syncRole->with('permissions')
+        ], Response::HTTP_CREATED);
+    }
+
+    function update_role(Request $req, $id)
+    {
+        $selected_role = Role::findOrFail($id);
+
+        $req->validate([
+            'role_name' => 'required|string|unique:roles,role_name,'.$id,
+        ]);
+
+        $selected_role->role_name = $req->input('role_name');
+        $selected_role->save();
+
+        return response()->json([
+            "msg" => $selected_role->role_name." saved successfully",
+            "data" => $selected_role
+        ], Response::HTTP_CREATED);
+    }
+
+    function destroy_role($id)
+    {
+        $selected_role = Role::withCount('permissions')->findOrFail($id);
+        if($selected_role->permissions_count > 0){
+            return response()->json([
+                "msg" => $selected_role->role_name." can't be deleted it has some permissions assigned",
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $selected_role->delete();
+        return response()->json([
+            "msg" => "selected role has been deletes successfully",
+        ], Response::HTTP_OK);
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,34 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
+use App\Http\Controllers\RolePermissionController;
 
 Route::get('/', function () {
     return response()->json([
         "msg" => "hinvite says hello"
     ], Response::HTTP_OK);
+});
+
+Route::group(['middleware' => 'api',  'prefix' => 'admin'], function ($router) {
+
+    Route::prefix('access_control')->group(function () {
+        Route::prefix('roles')->group(function () {
+            Route::get('/', [RolePermissionController::class, 'index']);
+            Route::post('/store', [RolePermissionController::class, 'store_role']);
+            // Route::get('/{id}/show', [RoleController::class, 'show']);
+            // Route::match(['post', 'put'],'/{id}/update', [RoleController::class, 'update']);
+            // Route::delete('/{id}/delete', [RoleController::class, 'destroy']);
+            // Route::post('/{id}/addPermissions', [RolePermissionController::class, 'addPermissions']);
+        });
+
+        // Route::prefix('permissions')->group(function () {
+        //     Route::get('/', [PermissionController::class, 'index']);
+        //     Route::post('/store', [PermissionController::class, 'store']);
+        //     Route::get('/{id}/show', [PermissionController::class, 'show']);
+        //     Route::match(['post', 'put'],'/{id}/update', [PermissionController::class, 'update']);
+        //     Route::delete('/{id}/delete', [PermissionController::class, 'destroy']);
+        // });
+
+//        Route::get('/', [RoleController::class, 'index']);
+    });
 });
