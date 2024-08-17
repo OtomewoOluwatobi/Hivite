34a4e9fc16e948490d234f799b7dc93e9acd3246
Otomewo Oluwatobi. E <otomewooluwatobi@gmail.com>
2024-08-17 04:50:15 +0100

    CRUD functionality for roles and permission fixed

--- app/Http/Controllers/RolePermissionController.php
+++ app/Http/Controllers/RolePermissionController.php
@@ -10,10 +10,10 @@ use Spatie\Permission\Models\Permission;
 
 class RolePermissionController extends Controller
 {
-    public function __construct(){
-    }
+    public function __construct() {}
 
-    function create(Request $req){
+    function create(Request $req)
+    {
         $allPermissions = Permission::select('name', 'id')->get();
         $allRoles = Role::with('permissions')->withCount('users')->get();
 
@@ -31,7 +31,8 @@ class RolePermissionController extends Controller
         ], Response::HTTP_OK);
     }
 
-    function store_role(Request $req){
+    function store_role(Request $req)
+    {
         $req->validate([
             'name' => 'required|string|unique:roles,name',
             "permission_ids" => "required|array",
@@ -49,19 +50,28 @@ class RolePermissionController extends Controller
         ], Response::HTTP_CREATED);
     }
 
+    function show_role($id)
+    {
+        $selected_role = Role::with('permission', 'users')->find($id);
+
+        return response()->json([
+            "data" => $selected_role
+        ], Response::HTTP_OK);
+    }
+
     function update_role(Request $req, $id)
     {
         $selected_role = Role::findOrFail($id);
 
         $req->validate([
-            'role_name' => 'required|string|unique:roles,role_name,'.$id,
+            'role_name' => 'required|string|unique:roles,role_name,' . $id,
         ]);
 
         $selected_role->role_name = $req->input('role_name');
         $selected_role->save();
 
         return response()->json([
-            "msg" => $selected_role->role_name." saved successfully",
+            "msg" => $selected_role->role_name . " saved successfully",
             "data" => $selected_role
         ], Response::HTTP_CREATED);
     }
@@ -69,9 +79,9 @@ class RolePermissionController extends Controller
     function destroy_role($id)
     {
         $selected_role = Role::withCount('permissions')->findOrFail($id);
-        if($selected_role->permissions_count > 0){
+        if ($selected_role->permissions_count > 0) {
             return response()->json([
-                "msg" => $selected_role->role_name." can't be deleted it has some permissions assigned",
+                "msg" => $selected_role->role_name . " can't be deleted it has some permissions assigned",
             ], Response::HTTP_UNAUTHORIZED);
         }
         $selected_role->delete();
@@ -81,4 +91,79 @@ class RolePermissionController extends Controller
     }
 
 
+    function index_permission(Request $req)
+    {
+        $permissions = Permission::with('roles')->withCount('users')->get();
+        return response()->json([
+            $permissions
+        ], Response::HTTP_OK);
+    }
+
+    function store_permission(Request $req, Response $res)
+    {
+        $req->validate([
+            'permissions' => 'required|array',
+            'permissions.*' => 'required|string|unique:permissions,name',
+        ]);
+
+        $newPermissions = [];
+
+        foreach ($req->permissions as $key => $permission) {
+            $permissions = [];
+
+            $permissions['guard_name'] = strtolower("api");
+            $permissions['name'] = strtolower($permission);
+            $permissions['created_at'] = now();
+            $permissions['updated_at'] = now();
+
+            array_push($newPermissions, $permissions);
+        }
+
+        Permission::insert($newPermissions);
+
+        return response()->json([
+            "msg" => "permissions created successfully",
+            "data" => $newPermissions
+        ], Response::HTTP_CREATED);
+    }
+
+    function show_permission($id)
+    {
+        $selected_permission = Permission::with('roles', 'users')->find($id);
+
+        return response()->json([
+            "data" => $selected_permission
+        ], Response::HTTP_OK);
+    }
+
+    function update_permission(Request $req, $id)
+    {
+        $selected_permission = Permission::findOrFail($id);
+
+        $req->validate([
+            'name' => 'required|string|unique:roles,name,' . $id,
+        ]);
+
+        $selected_permission->name = $req->input('name');
+        $selected_permission->save();
+
+        return response()->json([
+            "msg" => $selected_permission->name . " saved successfully",
+            "data" => $selected_permission
+        ], Response::HTTP_CREATED);
+    }
+
+    function destroy_permission($id)
+    {
+        $selected_permission = Permission::withCount('permissions')->findOrFail($id);
+        if ($selected_permission->roles_count > 0) {
+            return response()->json([
+                "msg" => $selected_permission->name . " can't be deleted it assigned to a role",
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $selected_permission->delete();
+        return response()->json([
+            "msg" => "selected permission has been deletes successfully",
+        ], Response::HTTP_OK);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,22 @@ Route::get('/', function () {
 Route::group(['middleware' => 'api',  'prefix' => 'admin'], function ($router) {
 
     Route::prefix('access_control')->group(function () {
+
+        Route::get('/create', [RolePermissionController::class, 'create']);
+
         Route::prefix('roles')->group(function () {
             Route::get('/', [RolePermissionController::class, 'index_role']);
             Route::post('/store', [RolePermissionController::class, 'store_role']);
+            Route::get('/{id}/show', [RolePermissionController::class, 'show_role']);
+            Route::match(['post', 'put'],'/{id}/update', [RolePermissionController::class, 'update_role']);
+            Route::delete('/{id}/delete', [RolePermissionController::class, 'destroy_role']);
         });
+    });
+    Route::prefix('permissions')->group(function () {
+        Route::get('/', [RolePermissionController::class, 'index_permission']);
+        Route::post('/store', [RolePermissionController::class, 'store_permission']);
+        Route::get('/{id}/show', [RolePermissionController::class, 'show_permission']);
+        Route::match(['post', 'put'],'/{id}/update', [RolePermissionController::class, 'update_permission']);
+        Route::delete('/{id}/delete', [RolePermissionController::class, 'destroy_permission']);
     });
 });
